27c7aad3db8e5ab1f3ae85c18a573be37dc576c7
ding-co <pangoons@naver.com>
2023-03-05 22:08:37 +0900

    feat: add HttpMethod enum, HttpRequest struct

--- http-server/src/main.rs
+++ http-server/src/main.rs
@@ -2,6 +2,12 @@ use constants::SERVER_ADDRESS;
 
 mod constants;
 
+/* Request example
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
+
 fn main() {
     let server = Server::new(SERVER_ADDRESS.to_string());
     server.run();
@@ -20,3 +26,23 @@ impl Server {
         println!("Listening on {}", self.addr);
     }
 }
+
+enum HttpMethod {
+    GET,
+    POST,
+    PUT,
+    PATCH,
+    DELETE,
+    OPTIONS,
+    HEAD,
+    CONNECT,
+    TRACE,
+}
+
+struct HttpRequest {
+    method: HttpMethod,
+    path: String,
+    query_string: Option<String>,
+}
+
+impl HttpRequest {}
